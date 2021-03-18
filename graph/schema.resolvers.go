4ff7943e270c25dc15a66979ee0abf17542e32a8
Linielson <linielsonrosa@gmail.com.br>
2021-03-18 07:21:29 -0300

    Create query lazy loading

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -12,10 +12,36 @@ import (
 	"github.com/linielson/fc2-graphql/graph/model"
 )
 
+func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]*model.Course, error) {
+	var courses []*model.Course
+
+	// in DB: query Courses where Category ID ==
+	for _, v := range r.Resolver.Courses {
+		if v.Category.ID == obj.ID {
+			courses = append(courses, v)
+		}
+	}
+
+	return courses, nil
+}
+
+func (r *courseResolver) Chapters(ctx context.Context, obj *model.Course) ([]*model.Chapter, error) {
+	var chapters []*model.Chapter
+
+	// in DB: query Chapters where Course ID ==
+	for _, v := range r.Resolver.Chapters {
+		if v.Course.ID == obj.ID {
+			chapters = append(chapters, v)
+		}
+	}
+
+	return chapters, nil
+}
+
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
 	category := model.Category{
-		ID: fmt.Sprintf("T%d", rand.Int()),
-		Name: input.Name,
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,
 		Description: &input.Description,
 	}
 
@@ -32,10 +58,10 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCour
 	}
 
 	course := model.Course{
-		ID: fmt.Sprintf("T%d", rand.Int()),
-		Name: input.Name,
+		ID:          fmt.Sprintf("T%d", rand.Int()),
+		Name:        input.Name,
 		Description: &input.Description,
-		Category: category,
+		Category:    category,
 	}
 
 	r.Courses = append(r.Courses, &course)
@@ -51,14 +77,13 @@ func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewCha
 	}
 
 	chapter := model.Chapter{
-		ID: fmt.Sprintf("T%d", rand.Int()),
-		Name: input.Name,
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		Name:   input.Name,
 		Course: course,
 	}
 
 	r.Chapters = append(r.Chapters, &chapter)
 	return &chapter, nil
-
 }
 
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
@@ -73,11 +98,19 @@ func (r *queryResolver) Chapters(ctx context.Context) ([]*model.Chapter, error) 
 	return r.Resolver.Chapters, nil
 }
 
+// Category returns generated.CategoryResolver implementation.
+func (r *Resolver) Category() generated.CategoryResolver { return &categoryResolver{r} }
+
+// Course returns generated.CourseResolver implementation.
+func (r *Resolver) Course() generated.CourseResolver { return &courseResolver{r} }
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+type categoryResolver struct{ *Resolver }
+type courseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
